9e139b9a01375d1bc91140f9d280ba2bc5550d1f
Majo <majoosorio2402@gmail.com>
2023-05-15 23:19:43 -0500

    Fix platform issues and animation warning

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platforms : MonoBehaviour
+{
+    [SerializeField] private float speed; // The speed at which the platform moves
+    [SerializeField] private float distance; // The distance the platform can move
+    [SerializeField] private DirectionType direction; // The direction in which the platform moves
+    private Vector3 movement; // The vector direction of the movement
+    private Vector3 initialPos; // The initial position of the platform
+
+    // Enum to specify the direction of the platform movement
+    public enum DirectionType
+    {
+        Vertical,
+        Horizontal
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // If the colliding object has the tag "Player", set the platform as its parent
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.collider.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        // If the object leaving the collision has the tag "Player", remove the platform as its parent
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.collider.transform.SetParent(null);
+        }
+    }
+
+    private void Awake()
+    {
+        // Store the initial position of the platform
+        initialPos = transform.position;
+
+        // Set the movement direction based on the direction type
+        movement = direction == DirectionType.Vertical ? Vector3.up : Vector3.right;
+    }
+
+    void Update()
+    {
+        // Calculate the offset for the platform movement
+        float offset = Mathf.Sin(Time.time * speed) * distance;
+        // Update the platform position
+        transform.position = initialPos + movement * offset;
+    }
+}
