0fb1e217e495a1e0789367b1aea51e43474efdbe
Laura Domínguez <laura.dom97@hotmail.com>
2023-05-16 20:58:57 -0500

    Cambios nuevos

---
+++ Assets/Scripts/PanelController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PanelController : MonoBehaviour
+{
+    public GameObject panel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PanelDuration(4f));
+
+
+    }
+
+
+
+    IEnumerator PanelDuration(float tiempo)
+    {
+        yield return new WaitForSeconds(tiempo);
+        panel.SetActive(false);
+    }
+
+
+}
