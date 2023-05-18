2f2b58e8a206e417463b08fcb9c57168e08541d0
JisusTQ <jesus.torresq@udea.edu.co>
2023-05-18 14:13:21 -0500

    add qulity settings

--- Assets/Scripts/BrightnessSettings.cs
+++ Assets/Scripts/BrightnessSettings.cs
@@ -12,15 +12,10 @@ public class BrightnessSettings : MonoBehaviour
     void Start()
     {
         sliderValue = PlayerPrefs.GetFloat("brightness", 0.5f);
-
         panelBrightness.color = new Color(panelBrightness.color.r, panelBrightness.color.g, panelBrightness.color.b, sliderValue);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ 
 
     public void ChangeSlider(float value)
     {
