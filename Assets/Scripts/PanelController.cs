cbadae8db48dadccbc86c4858ce923344ecb066a
JisusTQ <jesus.torresq@udea.edu.co>
2023-05-19 20:09:53 -0500

    some screens

--- Assets/Scripts/PanelController.cs
+++ Assets/Scripts/PanelController.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 
 public class PanelController : MonoBehaviour
 {
-    public GameObject panel;
+    [SerializeField] GameObject panel;
+    [SerializeField] GameObject panelESC;
 
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(PanelDuration(4f));
-
-
+        StartCoroutine(PanelDuration2(4f));
     }
-
-
 
     IEnumerator PanelDuration(float tiempo)
     {
         yield return new WaitForSeconds(tiempo);
         panel.SetActive(false);
+        
+    }
+    IEnumerator PanelDuration2(float tiempo)
+    {
+        yield return new WaitForSeconds(tiempo);
+        panelESC.SetActive(true);
+        yield return new WaitForSeconds(tiempo);
+        panelESC.SetActive(false);
     }
 
 
