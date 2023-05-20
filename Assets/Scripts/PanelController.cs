a7e6858d5dbb24e968901c2f203bae845277adfd
JisusTQ <jesus.torresq@udea.edu.co>
2023-05-20 18:37:35 -0500

    change born scene

--- Assets/Scripts/PanelController.cs
+++ Assets/Scripts/PanelController.cs
@@ -2,18 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PanelController : MonoBehaviour
 {
     [SerializeField] GameObject panel;
     [SerializeField] GameObject panelESC;
+    [SerializeField] GameObject[] credits;
     [SerializeField] Animator fade;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(PanelDuration(6f));
+        if(SceneManager.GetActiveScene().name=="Red World")
+        {
+            StartCoroutine(PanelDuration(6f));
+        }
+        else if (SceneManager.GetActiveScene().name == "born")
+        {
+            StartCoroutine(Credits(8f));
+        }
     }
 
     IEnumerator PanelDuration(float tiempo)
@@ -26,11 +35,34 @@ public class PanelController : MonoBehaviour
         yield return new WaitForSeconds(tiempo);
         panelESC.SetActive(false);
     }
-    
+    IEnumerator Credits(float tiempo)
+    {
+        Invoke("FadeOutText", tiempo - 2);
+        yield return new WaitForSeconds(tiempo);
+        Invoke("FadeOutText", tiempo - 2);
+        credits[0].SetActive(false);
+        credits[1].SetActive(true);
+        yield return new WaitForSeconds(tiempo);
+        Invoke("FadeOutText", tiempo - 2);
+        credits[1].SetActive(false);
+        credits[2].SetActive(true);
+        yield return new WaitForSeconds(tiempo);
+        Invoke("FadeOutText", tiempo - 2);
+        credits[2].SetActive(false);
+        credits[3].SetActive(true);
+        yield return new WaitForSeconds(tiempo);
+        Invoke("FadeOutText", tiempo - 2);
+        credits[3].SetActive(false);
+        SceneManager.LoadScene("Red World");
+    }
 
     public void FadeOut()
     {
         fade.Play("FadeOut");
+    }
+    public void FadeOutText()
+    {
+        fade.Play("FadeOutText");
     }
 
 
