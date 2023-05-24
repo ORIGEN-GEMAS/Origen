46c08592b499feb6ba9b99a80103eaf185b17010
christianrruiz <88356596+christianrruiz@users.noreply.github.com>
2023-05-23 19:48:18 -0500

    zoomIn

---
+++ Assets/Scripts/ZoomInFOV.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using Cinemachine;
+
+public class ZoomInFOV: MonoBehaviour
+{
+    public Transform target;
+    public float zoomSpeed = 2f;
+    public float maxZoom = 30f;
+    public float minZoom = 60f;
+    public float zoomThreshold = 5f;
+
+    private CinemachineVirtualCamera virtualCamera;
+    private float initialFov;
+    private bool isZooming = false;
+
+    private void Start()
+    {
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        initialFov = virtualCamera.m_Lens.FieldOfView;
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        if (distance < zoomThreshold && !isZooming)
+        {
+            isZooming = true;
+            StartCoroutine(ZoomIn());
+        }
+        else if (distance >= zoomThreshold && isZooming)
+        {
+            isZooming = false;
+            StartCoroutine(ZoomOut());
+        }
+    }
+
+    private System.Collections.IEnumerator ZoomIn()
+    {
+        float currentFov = virtualCamera.m_Lens.FieldOfView;
+
+        while (currentFov > maxZoom)
+        {
+            currentFov -= zoomSpeed * Time.deltaTime;
+            currentFov = Mathf.Clamp(currentFov, maxZoom, initialFov);
+            virtualCamera.m_Lens.FieldOfView = currentFov;
+            yield return null;
+        }
+    }
+
+    private System.Collections.IEnumerator ZoomOut()
+    {
+        float currentFov = virtualCamera.m_Lens.FieldOfView;
+
+        while (currentFov < minZoom)
+        {
+            currentFov += zoomSpeed * Time.deltaTime;
+            currentFov = Mathf.Clamp(currentFov, initialFov, minZoom);
+            virtualCamera.m_Lens.FieldOfView = currentFov;
+            yield return null;
+        }
+    }
+}
